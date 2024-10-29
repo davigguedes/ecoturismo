47e27027622085d9e8ce5e42c9525c6e8ccd08d3
Lilith Fernandes <166145996+LilithFernandesPat@users.noreply.github.com>
2024-10-29 20:03:47 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
     let items = carousel.querySelectorAll(".item");
     let dotsContainer = document.querySelector(".dots");
     let odsSection = document.querySelector(".ods");
-  
-    // Lista de gradientes para cada item
     let gradients = [
       "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 24%, rgba(0,212,255,1) 100%)", // Gradiente 1
-      "linear-gradient(90deg, rgba(11,105,13,1) 0%, rgba(15,148,15,1) 34%, rgba(72,249,1,1) 100%)", // Gradiente 2 (Corrigido)
+      "linear-gradient(90deg, rgba(11,105,13,1) 0%, rgba(15,148,15,1) 34%, rgba(72,249,1,1) 100%)", // Gradiente 2
       "linear-gradient(90deg, rgba(105,11,31,1) 0%, rgba(217,15,64,1) 42%, rgba(240,134,134,1) 100%)", // Gradiente 3
       "linear-gradient(90deg, rgba(195,103,0,1) 0%, rgba(255,158,51,1) 42%, rgba(247,218,105,1) 100%)", // Gradiente 4
       "linear-gradient(90deg, rgba(0,2,195,1) 0%, rgba(51,149,255,1) 43%, rgba(105,245,247,1) 100%)", // Gradiente 5
@@ -36,7 +34,6 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       });
   
-      // Aplicando o gradiente no background da seção .ods
       odsSection.style.backgroundImage = gradients[index];
     }
   
