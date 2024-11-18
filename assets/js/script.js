7d8216a911a82997ce4c0a8d7f50ce55b59672c0
humbertosamora <humberto.samora@gmail.com>
2024-11-18 17:14:06 -0300

    Atualizado com arquivos da Lilith

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,27 @@ document.addEventListener("DOMContentLoaded", function () {
     let items = carousel.querySelectorAll(".item");
     let dotsContainer = document.querySelector(".dots");
     let odsSection = document.querySelector(".ods");
-    let gradients = [
-      "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 24%, rgba(0,212,255,1) 100%)", // Gradiente 1
-      "linear-gradient(90deg, rgba(11,105,13,1) 0%, rgba(15,148,15,1) 34%, rgba(72,249,1,1) 100%)", // Gradiente 2
-      "linear-gradient(90deg, rgba(105,11,31,1) 0%, rgba(217,15,64,1) 42%, rgba(240,134,134,1) 100%)", // Gradiente 3
-      "linear-gradient(90deg, rgba(195,103,0,1) 0%, rgba(255,158,51,1) 42%, rgba(247,218,105,1) 100%)", // Gradiente 4
-      "linear-gradient(90deg, rgba(0,2,195,1) 0%, rgba(51,149,255,1) 43%, rgba(105,245,247,1) 100%)", // Gradiente 5
-      "linear-gradient(45deg, #33FFD7, #A9FF33)"  // Gradiente 6
-    ];
+    let odsImg = document.getElementById("odsimg");
   
-    // Inserindo dots
+    // background gradientes
+    let gradients = [
+      "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 24%, rgba(0,212,255,1) 100%)", 
+      "linear-gradient(90deg, rgba(11,105,13,1) 0%, rgba(15,148,15,1) 34%, rgba(72,249,1,1) 100%)", 
+      "linear-gradient(90deg, rgba(105,11,31,1) 0%, rgba(217,15,64,1) 42%, rgba(240,134,134,1) 100%)", 
+      "linear-gradient(90deg, rgba(195,103,0,1) 0%, rgba(255,158,51,1) 42%, rgba(247,218,105,1) 100%)",
+      "linear-gradient(90deg, rgba(0,2,195,1) 0%, rgba(51,149,255,1) 43%, rgba(105,245,247,1) 100%)", 
+      "linear-gradient(45deg, #33FFD7, #A9FF33)"  
+    ];
+    //images ods
+    const images = [
+      "assets/img/blue.png",
+      "assets/img/green.png",
+      "assets/img/pink.png",
+      "assets/img/orange.png",
+      "assets/img/azulagua.png",
+      "assets/img/verdefloresta.png"
+  ];
+
     items.forEach((_, index) => {
       let dot = document.createElement("span");
       dot.classList.add("dot");
@@ -22,9 +33,24 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   
     let dots = document.querySelectorAll(".dot");
-  
-    // Função para mostrar um item e mudar o background para gradiente
+    
+
     function showItem(index) {
+
+      //opacidade ease out
+      odsSection.style.opacity = 0;
+      odsImg.classList.add("rotate");
+
+      setTimeout(() => {
+        items.forEach((item, idx) => {
+            item.classList.remove("active");
+            dots[idx].classList.remove("active");
+            if (idx === index) {
+                item.classList.add("active");
+                dots[idx].classList.add("active");
+            }
+        });
+
       items.forEach((item, idx) => {
         item.classList.remove("active");
         dots[idx].classList.remove("active");
@@ -33,8 +59,21 @@ document.addEventListener("DOMContentLoaded", function () {
           dots[idx].classList.add("active");
         }
       });
-  
+      
+      //mudar background
       odsSection.style.backgroundImage = gradients[index];
+      //mudar imagem
+      odsImg.src = images[index];
+
+      // opacidade ease in
+      odsSection.style.opacity = 1;
+    }, 500); //tempo
+    
+    //animação rotação
+    odsImg.addEventListener("transitionend", () => {
+      odsImg.classList.remove("rotate");
+  }, { once: true });
+
     }
   
     // Navegação com botões
@@ -60,34 +99,71 @@ document.addEventListener("DOMContentLoaded", function () {
     showItem(0);
   });
   
- 
-function carregarDicas() {
-  fetch('/assets/json/dicas.json')
-    .then(response => response.json()) 
-    .then(data => {
-      const dicaContainer = document.getElementById('dica');
-      
-      data.forEach(dicaItem => {
-        // Para cada dicaItem, criamos uma card
-        const cardHTML = `
-          <div class="card" style="width: 18rem; height: 28rem; background-color: white; margin: 10px;">
-            <div class="card-body">
-              <h5 class="card-title" style="text-align: center; color: #4e537e; font-size: x-large;">${dicaItem.titulo}</h5>
-              <p class="card-text" style="color: #27644a; font-size: larger; padding-left: 15px;">
-                ${dicaItem.dicas.join('<br>')}
-              </p>
-              <a href="${dicaItem.botao}" target="_blank" style="color: green; text-decoration: none; display: block; text-align: center; font-size: x-large;" class="card-link">
-                Saiba Mais
-              </a>
-            </div>
-          </div>
-        `;
+  function carregarDicas() {
+    fetch('/assets/json/dicas.json')
+      .then(response => response.json()) 
+      .then(data => {
+        const dicaContainer = document.getElementById('boxdicas');
         
-        // Adiciona o HTML da card ao container
-        dicaContainer.innerHTML += cardHTML;
-      });
-    })
-    .catch(error => console.error('Erro ao carregar o JSON:', error));
-}
+        dicaContainer.innerHTML = '';
 
-document.addEventListener('DOMContentLoaded', carregarDicas);
+        data.forEach(dicaItem => {
+          // Para cada dicaItem, criamos uma card
+          const cardHTML = `
+            <div class="card dica" style="width: 18rem; height: 28rem; margin: 10px;">
+              <div class="card-body">
+                <h5 class="card-title" style="text-align: center; color: #4e537e; font-size: x-large;">${dicaItem.titulo}</h5>
+                <p class="card-text" style="color: #27644a; font-size: larger; padding-left: 15px;">
+                  ${dicaItem.dicas.join('<br>')}
+                </p>
+                <a href="${dicaItem.botao}" target="_blank" style="color: green; text-decoration: none; display: block; text-align: center; font-size: x-large;" class="card-link">
+                  Saiba Mais
+                </a>
+              </div>
+            </div>
+          `;
+          
+          // Adiciona o HTML da card ao container
+          dicaContainer.innerHTML += cardHTML;
+        });
+      })
+      .catch(error => console.error('Erro ao carregar o JSON:', error));
+  }
+  
+  document.addEventListener('DOMContentLoaded', carregarDicas);
+
+  const showOutfit = () =>{
+    document.getElementById("first").style.display = 'block';
+    document.getElementById("second").style.display = 'none';
+    document.getElementById("third").style.display = 'none';
+    document.getElementById("four").style.display = 'none';
+    document.getElementById("five").style.display = 'none';
+  }
+  const showOutfit2 = () =>{
+    document.getElementById("first").style.display = 'none';
+    document.getElementById("second").style.display = 'block';
+    document.getElementById("third").style.display = 'none';
+    document.getElementById("four").style.display = 'none';
+    document.getElementById("five").style.display = 'none';
+  }
+  const showOutfit3 = () =>{
+    document.getElementById("first").style.display = 'none';
+    document.getElementById("second").style.display = 'none';
+    document.getElementById("third").style.display = 'block';
+    document.getElementById("four").style.display = 'none';
+    document.getElementById("five").style.display = 'none';
+  }
+  const showOutfit4 = () =>{
+    document.getElementById("first").style.display = 'none';
+    document.getElementById("second").style.display = 'none';
+    document.getElementById("third").style.display = 'none';
+    document.getElementById("four").style.display = 'block';
+    document.getElementById("five").style.display = 'none';
+  }
+  const showOutfit5 = () =>{
+    document.getElementById("first").style.display = 'none';
+    document.getElementById("second").style.display = 'none';
+    document.getElementById("third").style.display = 'none';
+    document.getElementById("four").style.display = 'none';
+    document.getElementById("five").style.display = 'block';
+  }
